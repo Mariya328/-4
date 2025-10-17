4269787f76ed4b2128057026ba4225e6e08464cb
Студент <marisendikaeva@mail.ru>
2025-10-17 11:03:05 +0300

    commit1

---
+++ Lab1.1/Lab1/Lab1.1.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    printf("Hello world!\n"); // 1.1.0
+
+    printf("Гараев\n"); // 1.1.1
+    printf("Максим\n");
+    printf("Русланович\n");
+    printf("\n");
+    printf("Группа: ИСЭбд-11\n");
+    printf("Дата: 02.09.2025\n");
+    printf("Предмет: Основы программирования\n");
+    printf("Лаб №1\n");
+    printf("\n");
+
+    printf("Лаб №1\n"); //1.1.2
+    printf("Предмет: Основы программирования\n");
+    printf("Дата: 02.09.2025\n");
+    printf("Группа: ИСЭбд-11\n");
+    printf("\n");
+    printf("Русланович\n");
+    printf("Максим\n");
+    printf("Гараев\n");
+
+    printf("  /\\\n"); //1.1.4
+    printf(" /  \\\n");
+    printf("/____\\\n");
+    printf("|    |\n");
+    printf("|____|\n");
+    printf(" |  |\n");
+
+    return 0;
+}
---
+++ Lab1.1/Lab1/Lab1.2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    printf("TEST");
+
+    return 0;
+}
